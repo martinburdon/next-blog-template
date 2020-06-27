f87d516c74400158f792fabb3e5888c0b9d77bc3
Martin Burdon <martin@martinburdon.co.uk>
2020-06-27 22:26:35 +0100

    Basic styles

--- components/Header.js
+++ components/Header.js
@@ -5,14 +5,36 @@ export default function Header() {
     <>
       <header className="header">
         <nav className="nav">
-          <Link href="/">
-            <a>My Blog</a>
-          </Link>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>My Blog</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
         </nav>
       </header>
+      <style jsx>{`
+        header {
+          display: flex;
+          justify-content: center;
+          padding: 2rem;
+        }
+
+        ul {
+          display: flex;
+          list-style: none;
+        }
+
+        li {
+          margin: 0 1rem;
+        }
+      `}</style>
     </>
   )
 }
--- components/Layout.js
+++ components/Layout.js
@@ -9,12 +9,65 @@ export default function Layout({ children, description, pageTitle, ...props }) {
         <meta charSet="utf-8" />
         <meta name="description" content={description}></meta>
         <title>{pageTitle}</title>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;800;900&display=swap" rel="stylesheet" />
       </Head>
-      <section className="layout">
-        <Header />
-        <div className="content">{children}</div>
-      </section>
-      <footer>Built by me!</footer>
+      <style jsx global>{`
+        html {
+          font-size: 10px;
+        }
+
+        body {
+          font-size: 1.6rem;
+        }
+
+        html,
+        body {
+          margin: 0;
+          padding: 0;
+          font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+            Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+            sans-serif;
+          color: #445566;
+        }
+
+        a {
+          color: #4caf50;
+        }
+
+        a:hover {
+          color: #3a833c;
+        }
+
+        img {
+          height: auto;
+          max-width: 100%;
+        }
+
+        .site {
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
+        }
+
+        .layout,
+        footer {
+          padding: 2rem;
+        }
+
+        footer {
+          background: #eee;
+          display: flex;
+          justify-content: center;
+          margin-top: auto;
+        }
+      `}</style>
+      <div className="site">
+        <section className="layout">
+          <Header />
+          <div className="content">{children}</div>
+        </section>
+        <footer>Built by me!</footer>
+      </div>
     </>
   )
 }
